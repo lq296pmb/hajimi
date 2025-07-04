82a5bc2bc6644f14060d136a29f2d6fda5d22c07
lq296pmb <lq296pmb@outlook.com>
2025-07-04 20:39:54 +0800

    Update settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -25,12 +25,12 @@ ENABLE_STORAGE = os.environ.get("ENABLE_STORAGE", "false").lower() in ["true", "
 # 并发请求配置
 CONCURRENT_REQUESTS = int(os.environ.get("CONCURRENT_REQUESTS", "1"))  # 默认并发请求数
 INCREASE_CONCURRENT_ON_FAILURE = int(os.environ.get("INCREASE_CONCURRENT_ON_FAILURE", "0"))  # 失败时增加的并发数
-MAX_CONCURRENT_REQUESTS = int(os.environ.get("MAX_CONCURRENT_REQUESTS", "3"))  # 最大并发请求数
+MAX_CONCURRENT_REQUESTS = int(os.environ.get("MAX_CONCURRENT_REQUESTS", "1"))  # 最大并发请求数
 
 # 缓存配置
-CACHE_EXPIRY_TIME = int(os.environ.get("CACHE_EXPIRY_TIME", "21600"))  # 默认缓存 6 小时 (21600 秒)
-MAX_CACHE_ENTRIES = int(os.environ.get("MAX_CACHE_ENTRIES", "500"))  # 默认最多缓存500条响应
-CALCULATE_CACHE_ENTRIES = int(os.environ.get("CALCULATE_CACHE_ENTRIES", "6"))  # 默认取最后 6 条消息算缓存键
+CACHE_EXPIRY_TIME = int(os.environ.get("CACHE_EXPIRY_TIME", "1"))  # 默认缓存 6 小时 (21600 秒)
+MAX_CACHE_ENTRIES = int(os.environ.get("MAX_CACHE_ENTRIES", "0"))  # 默认最多缓存500条响应
+CALCULATE_CACHE_ENTRIES = int(os.environ.get("CALCULATE_CACHE_ENTRIES", "0"))  # 默认取最后 6 条消息算缓存键
 PRECISE_CACHE = os.environ.get("PRECISE_CACHE", "false").lower() in ["true", "1", "yes"] #是否取所有消息来算缓存键
 
 # 是否启用 Vertex AI
